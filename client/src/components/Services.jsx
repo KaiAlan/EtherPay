c2f655467d05d2cb27988a32898b7270222e4bd8
Kaialan <satyajit1501@gmail.com>
2023-05-09 16:30:33 +0530

    🚀 readyy to deploy

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -21,7 +21,7 @@ const ServiceCard = ({color, title, icon, subtitle}) => (
 
 const Services = () => {
   return (
-    <div className='flex flex-col md:flex-row w-full justify-center items-center gradient-bg-services'>
+    <div className='flex w-full justify-center items-center gradient-bg-services'>
       <div className='flex mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4'>
         <div className='flex flex-1 flex-col justify-start items-start'>
           <h1 className='text-white text-3xl sm:text-5xl py-2 text-gradient'>Services that we
